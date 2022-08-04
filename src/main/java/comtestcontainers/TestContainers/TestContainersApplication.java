9c0968031d4d6999270ba098ad25e34a8908a6ea
Gabriel Alan <gabrielalan.development@outlook.com>
2022-08-04 19:30:02 -0300

    bootstrap

---
+++ src/main/java/comtestcontainers/TestContainers/TestContainersApplication.java
@@ -0,0 +1,13 @@
+package comtestcontainers.TestContainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestContainersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestContainersApplication.class, args);
+	}
+
+}
