e1fd5902dde2610556d0722471ef9fe0873adeb5
Gabriel Alan <gabrielalan.development@outlook.com>
2022-08-05 00:13:44 -0300

    Stable MySQL implementation!

--- src/test/java/comtestcontainers/TestContainers/service/BalanceServiceTest.java
+++ src/test/java/comtestcontainers/TestContainers/service/BalanceServiceTest.java
@@ -1,18 +1,15 @@
 package comtestcontainers.TestContainers.service;
 
-import comtestcontainers.TestContainers.TestContainersApplication;
 import comtestcontainers.TestContainers.containers.MySQLTestContainer;
-
 import comtestcontainers.TestContainers.model.Movement;
 import comtestcontainers.TestContainers.model.User;
 import comtestcontainers.TestContainers.repository.MovementRepository;
 import comtestcontainers.TestContainers.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -20,12 +17,9 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Testcontainers
-@ActiveProfiles("test")
-@ExtendWith(SpringExtension.class)
-@SpringBootTest(classes = TestContainersApplication.class)
+@SpringBootTest
 class BalanceServiceTest {
 
     @Container
@@ -40,16 +34,37 @@ class BalanceServiceTest {
     @Autowired
     private BalanceService balanceService;
 
-    @Test
-    void testTrue(){
+    private User user;
 
-        final var user = userRepository.save(new User("Gabriel"));
+    @BeforeEach
+    void setUp(){
+
+        movementRepository.deleteAll();
+        userRepository.deleteAll();
+
+        user = userRepository.save(new User("Gabriel"));
+
+    }
+
+    @Test
+    void givenAnUserAndThreeMovements_whenCallGetBalanceBy_ReturnsValueEqualsToTen() {
 
         movementRepository.save(new Movement(user, new BigDecimal("5.00")));
         movementRepository.save(new Movement(user, new BigDecimal("15.00")));
         movementRepository.save(new Movement(user, new BigDecimal("-10.00")));
 
         assertEquals(new BigDecimal("10.00"), balanceService.getBalanceBy(user));
+
+    }
+
+    @Test
+    void givenAnUserAndThreeMovements_whenCallGetBalanceBy_ReturnsValueEqualsToZero() {
+
+        movementRepository.save(new Movement(user, new BigDecimal("5.00")));
+        movementRepository.save(new Movement(user, new BigDecimal("15.00")));
+        movementRepository.save(new Movement(user, new BigDecimal("-20.00")));
+
+        assertEquals(new BigDecimal("00.00"), balanceService.getBalanceBy(user));
 
     }
 
---
+++ src/test/java/comtestcontainers/TestContainers/service/MockedBalanceServiceTest.java
@@ -0,0 +1,15 @@
+package comtestcontainers.TestContainers.service;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class MockedBalanceServiceTest {
+
+    @Test
+    void testSomething(){
+        assertTrue(true);
+    }
+
+}
